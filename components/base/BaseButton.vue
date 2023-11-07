a03851aa8ec4c35e73637f662c0a7f84563267bf
Jordy <jordy.pereira@hotmail.com>
2023-11-07 18:46:36 +0100

    wip

---
+++ components/base/BaseButton.vue
@@ -0,0 +1,14 @@
+
+import type { UButton } from '#ui-colors/components';
+<template>
+  <UButton primary v-bind="$attrs" v-on="$listeners">
+    <slot />
+  </UButton>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  square?: boolean;
+  fullWidth?: boolean;
+}()
+</script>
