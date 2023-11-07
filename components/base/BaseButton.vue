d34e5b217f9779952a6b7ae422e1ae0e9ad8b2b8
Jordy <jordy.pereira@hotmail.com>
2023-11-07 19:19:49 +0100

    add playground

--- components/base/BaseButton.vue
+++ components/base/BaseButton.vue
@@ -1,14 +1,12 @@
-
-import type { UButton } from '#ui-colors/components';
 <template>
-  <UButton primary v-bind="$attrs" v-on="$listeners">
+  <UButton v-bind="$attrs" v-on="$listeners">
     <slot />
   </UButton>
 </template>
 
 <script lang="ts" setup>
 defineProps<{
-  square?: boolean;
-  fullWidth?: boolean;
-}()
+  square?: boolean
+  fullWidth?: boolean
+}>()
 </script>
