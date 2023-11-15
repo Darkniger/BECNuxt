31811253a471f1189fc8b046d8e853bd84733183
Jordy <jordy.pereira@hotmail.com>
2023-11-15 08:46:50 +0100

    APP: Button (#3)
    * add playground

---
+++ components/base/BaseButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <UButton v-bind="$attrs" v-on="$listeners">
+    <slot />
+  </UButton>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  square?: boolean
+  fullWidth?: boolean
+}>()
+</script>
---
+++ pages/playground.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto px-4">
+    <h1>Button</h1>
+    <BaseButton>Button</BaseButton>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
+
